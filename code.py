6ba21a0feb08a7933d270daae85165144f1dece6
Rizwan Ahsan <rizahsan4@gmail.com>
2021-01-22 18:38:09 -0500

    fixed conflict and added .gitignore

--- code.py
+++ code.py
@@ -1,2 +1,7 @@
-def are_valid_groups(student_no, groups):
-	return student_no in groups
+#Rizwan's code
+# def are_valid_groups(student_no, groups):
+# 	return student_no in groups
+
+def are_valid_groups(student_numbers, groups):
+    for group in groups:
+        return all(number in group for number in student_numbers)
