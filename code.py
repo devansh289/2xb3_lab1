35a6f08de5e9e7be219d82cfe0aec52c9758bdba
Rizwan Ahsan <rizahsan4@gmail.com>
2021-01-22 19:09:45 -0500

    breaking the code

--- code.py
+++ code.py
@@ -4,4 +4,7 @@
 
 def are_valid_groups(student_numbers, groups):
     for group in groups:
+        print("I shall break your code :)")
         return all(number in group for number in student_numbers)
+
+Devansh is a big boy now.
