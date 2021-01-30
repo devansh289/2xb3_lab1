1548c25f06f0d575ff7692dfde52e726cc84274f
Rizwan Ahsan <rizahsan4@gmail.com>
2021-01-29 20:10:01 -0500

    added method for testing append

--- lab2_code.py
+++ lab2_code.py
@@ -9,12 +9,22 @@ def copy_time(list_to_copy):
 sample_list = []
 
 # Loop to time list with increasing number of integers
-for _ in range(500):
+def copy_test():
+    for _ in range(500):
 
-    start_time = timeit.default_timer()
-    copy_time(sample_list)
-    end_time = timeit.default_timer()
-    print(end_time - start_time)
+        start_time = timeit.default_timer()
+        copy_time(sample_list)
+        end_time = timeit.default_timer()
+        print(end_time - start_time)
 
-    sample_list.append(random.randrange(1000000))
+        sample_list.append(random.randrange(1000000))
 
+def append_test():
+    for i in range(1000000):
+        start = timeit.default_timer()
+        sample_list.append(random.randint(1,1000000))
+        end = timeit.default_timer()
+        with open("nums.txt", "a") as f:
+            f.write(f"{end - start}\n")
+
+append_test()
