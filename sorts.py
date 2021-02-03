88f8316c4a20aa5efc0434eaba782011297cd1ad
Devansh Sharma <devanshsharma289@gmail.com>
2021-02-03 14:05:57 -0500

    Updated file with lab3.py methods

--- sorts.py
+++ sorts.py
@@ -1,3 +1,5 @@
+import timeit, random, math
+
 def quicksort_helper(L,start,end):
     #The pivot element is chosen to be the last one in the list
     pivot_element = L[end]
@@ -29,4 +31,48 @@ def quicksort_inplace(L):
     # Only call quicksort_helper if list has atleast 2 elements
     if len(L)>1:
         quicksort_helper(L,0,len(L)-1)
-    else:
+
+def my_quicksort(L):
+    copy = quicksort_copy(L)
+    for i in range(len(L)):
+        L[i] = copy[i]
+
+def quicksort_copy(L):
+    if len(L) < 2:
+        return L
+    pivot = L[0]
+    left, right = [], []
+    for num in L[1:]:
+        if num < pivot:
+            left.append(num)
+        else:
+            right.append(num)
+    return quicksort_copy(left) + [pivot] + quicksort_copy(right)
+
+def create_random_list(n):
+    L = []
+    for _ in range(n):
+        L.append(random.randint(1,n))
+    return L
+
+def create_near_sorted_list(n, factor):
+    L = create_random_list(n)
+    L.sort()
+    for _ in range(math.ceil(n*factor)):
+        index1 = random.randint(0, n-1)
+        index2 = random.randint(0, n-1)
+        L[index1], L[index2] = L[index2], L[index1]
+    return L
+
+for i in range(10000):
+    f = open('quicksort.txt', 'a')
+
+    test_list = create_near_sorted_list(i, 0.1)
+
+    start_time = timeit.default_timer()
+    my_quicksort(test_list)
+    end_time = timeit.default_timer()
+
+    f.write(str(end_time - start_time) + '\n')
+
+    f.close()
