206e3537eff2be80f53650cc66a2d880eb6610aa
FardeenAfsar <afsarfardeen@gmail.com>
2021-02-04 12:39:42 -0500

    Added worst case section

--- code.py
+++ code.py
@@ -1,12 +1,16 @@
 import timeit
-from lab3, sorts import *
+from lab3 import *
+from sorts import *
+import sys
 
-#Calculate time for quicksort_inplace with random values
+# Calculate time for quicksort_inplace with random values
+
+
 def quicksort_random_test():
     f = open('quicksort.txt', 'a')
 
     for i in range(10000):
-    
+
         test_list = create_random_list(i)
 
         start_time = timeit.default_timer()
@@ -17,11 +21,13 @@ def quicksort_random_test():
 
     f.close()
 
-#Calculate time for my_quicksort with almost sorted values
+# Calculate time for my_quicksort with almost sorted values
+
+
 def quicksort_nearly_sorted_test():
     f = open('quicksort.txt', 'a')
     for i in range(10000):
-        
+
         test_list = create_near_sorted_list(i, 0.1)
 
         start_time = timeit.default_timer()
@@ -32,14 +38,16 @@ def quicksort_nearly_sorted_test():
 
     f.close()
 
-#Calculate time for quicksort_inplace with small lists
+# Calculate time for quicksort_inplace with small lists
+
+
 def quicksort_small_list_test():
     f = open('quicksort.txt', 'a')
 
     for i in range(20):
         values = []
         for _ in range(10000):
-        
+
             test_list = create_random_list(i)
 
             start_time = timeit.default_timer()
@@ -52,14 +60,16 @@ def quicksort_small_list_test():
         values = []
     f.close()
 
-#Calculate time for insertion_sort with small lists
+# Calculate time for insertion_sort with small lists
+
+
 def insertion_small_list_test():
     f = open('quicksort.txt', 'a')
 
     for i in range(20):
         values = []
         for _ in range(10000):
-        
+
             test_list = create_random_list(i)
 
             start_time = timeit.default_timer()
@@ -71,3 +81,61 @@ def insertion_small_list_test():
         f.write(str(sum(values)/len(values)) + '\n')
         values = []
     f.close()
+
+
+# Worst case analysis of quicksort
+sys.setrecursionlimit(10**6)
+
+
+def quicksort_worst_case_test():
+    f = open('worstcase.txt', 'a')
+
+    for n in range(10000):
+        test_list = create_random_list(n)
+        test_list.sort(reverse=True)
+
+        start_time = timeit.default_timer()
+        quicksort_inplace(test_list)
+        end_time = timeit.default_timer()
+
+        f.write(str(n) + '\t' + str(end_time - start_time) + '\r')
+    f.close()
+
+
+def quicksort_avg_case_test():
+    f = open('worstcase.txt', 'a')
+
+    for n in range(3500):
+        test_list = create_random_list(n)
+
+        start_time = timeit.default_timer()
+        quicksort_inplace(test_list)
+        end_time = timeit.default_timer()
+
+        f.write(str(n) + '\t' + str(end_time - start_time) + '\r')
+    f.close()
+
+
+def time_test(sort, L):
+    start_time = timeit.default_timer()
+    sort(L)
+    end_time = timeit.default_timer()
+    return end_time - start_time
+
+
+def near_sort_analysis():
+    f = open('worstcase.txt', 'a')
+
+    for n in range(1000):
+        test_list = create_near_sorted_list(1000, (n + 1) / 1000)
+
+        quick_sort_time = time_test(quicksort_inplace, test_list)
+        merge_sort_time = time_test(merge_sort, test_list)
+        tim_sort_time = time_test(sorted, test_list)
+
+        f.write(str(n) + '\t' + str(quick_sort_time) + '\t' +
+                str(merge_sort_time) + '\t' + str(tim_sort_time) + '\r')
+    f.close()
+
+
+near_sort_analysis()
--- sorts.py
+++ sorts.py
@@ -1,43 +1,49 @@
 """ Start of functions we created"""
 
 # In place quicksort implementation
-def quicksort_helper(L,start,end):
-    #The pivot element is chosen to be the last one in the list
+
+
+def quicksort_helper(L, start, end):
+    # The pivot element is chosen to be the last one in the list
     pivot_element = L[end]
-    
+
     # Pointer to keep track of value greater than pivot
     p1 = start
-    
-    #Check to see if array has atleast two element
+
+    # Check to see if array has atleast two element
     if start < end:
-        
-        for i in range(start,end+1):
+
+        for i in range(start, end+1):
             # If current value <= pivot then swap
-            if L[i]<=pivot_element:
+            if L[i] <= pivot_element:
                 L[p1], L[i] = L[i], L[p1]
-                
+
                 # Move pointer to next element
                 if i != end:
-                    p1+=1
-        
-        #Sort the left subarray
+                    p1 += 1
+
+        # Sort the left subarray
         quicksort_helper(L, start, p1-1)
-        
-        #Sort the right subarray
+
+        # Sort the right subarray
         quicksort_helper(L, p1+1, end)
     return L
-   
-def quicksort_inplace(L):
-    
-    # Only call quicksort_helper if list has atleast 2 elements
-    if len(L)>1:
-        quicksort_helper(L,0,len(L)-1)
 
-#Multi-pivot quicksort methods
+
+def quicksort_inplace(L):
+
+    # Only call quicksort_helper if list has atleast 2 elements
+    if len(L) > 1:
+        quicksort_helper(L, 0, len(L)-1)
+
+# Multi-pivot quicksort methods
+
+
 def dual_pivot_quicksort(L):
     copy = dual_copy(L)
     for i in range(len(L)):
         L[i] = copy[i]
+
 
 def dual_copy(L):
     if len(L) < 2:
@@ -60,10 +66,12 @@ def dual_copy(L):
 
     return dual_copy(left) + [pivot1] + dual_copy(middle) + [pivot2] + dual_copy(right)
 
+
 def tri_pivot_quicksort(L):
     copy = tri_copy(L)
     for i in range(len(L)):
         L[i] = copy[i]
+
 
 def tri_copy(L):
     if len(L) < 2:
@@ -73,7 +81,7 @@ def tri_copy(L):
             swap(L, 0, 1)
         return L
 
-    #swap values such that pivot1 <= pivot2 <= pivot3
+    # swap values such that pivot1 <= pivot2 <= pivot3
     swap_for_3(L)
 
     pivot1 = L[0]
@@ -93,8 +101,10 @@ def tri_copy(L):
     return (tri_copy(left) + [pivot1] + tri_copy(lmiddle) + [pivot2]
             + tri_copy(rmiddle) + [pivot3] + tri_copy(right))
 
+
 def swap(L, i, j):
     L[i], L[j] = L[j], L[i]
+
 
 def swap_for_3(L):
     if L[0] > L[1]:
@@ -104,15 +114,18 @@ def swap_for_3(L):
     if L[1] > L[-1]:
         swap(L, 1, -1)
 
+
 def quad_pivot_quicksort(L):
     copy = quad_copy(L)
     for i in range(len(L)):
         L[i] = copy[i]
 
+
 def sort_pivot4(L, i, j, k, l):
     a = [i, j, k, l]
     a.sort()
     L[0], L[1], L[-2], L[-1] = a[0], a[1], a[2], a[3]
+
 
 def quad_copy(L):
     if len(L) < 2:
@@ -148,29 +161,71 @@ def quad_copy(L):
             + quad_copy(rr) + [pivot4] + quad_copy(r))
 
 # Implementation of Insertion Sort
+
+
 def insertion_sort(L):
-    for i in range(1,len(L)):
-    
+    for i in range(1, len(L)):
+
         # Value to compare others compare against
         currentvalue = L[i]
         p1 = i
-        
-        #Swap previous values which are smaller
+
+        # Swap previous values which are smaller
         while p1 >= 1 and L[p1-1] > currentvalue:
             L[p1] = L[p1-1]
             p1 -= 1
-        
+
         # Swap end as it has not been taken care of yet
         L[p1] = currentvalue
 
 # Final sort Implementation
+
+
 def finalSort(L):
     # Call quicksort_helper if list has more than 15 elements
     # Call insertion_sort if list has between 2 and 15 elements
     # List is already sorted if less than 2 elements
     if len(L) > 15:
-        quicksort_helper(L,0,len(L)-1)
+        quicksort_helper(L, 0, len(L)-1)
     elif len(L) > 1:
         insertion_sort(L)
+
+
+# Merge sort Implementation
+
+
+def merge_sort(L):
+    if len(L) > 1:
+        mid = len(L)//2
+        left_division = L[:mid]
+        right_division = L[mid:]
+
+        merge_sort(left_division)
+        merge_sort(right_division)
+
+        # Array pointers
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left_division) and j < len(right_division):
+            if left_division[i] < right_division[j]:
+                L[k] = left_division[i]
+                i += 1
+            else:
+                L[k] = right_division[j]
+                j += 1
+            k += 1
+
+        # Copying leftover array
+        while i < len(left_division):
+            L[k] = left_division[i]
+            i += 1
+            k += 1
+        while j < len(right_division):
+            L[k] = right_division[j]
+            j += 1
+            k += 1
+
 
 ''' End of functions we created'''
