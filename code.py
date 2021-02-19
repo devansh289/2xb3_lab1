abef740d207efa3b40e8be69ba98407c803eb2a0
FardeenAfsar <afsarfardeen@gmail.com>
2021-02-19 12:35:55 -0500

    Added 3-way mergesort

--- code.py
+++ code.py
@@ -1,4 +1,40 @@
 import timeit
-from lab3 import *
+from lab4 import *
 from sorts import *
 import sys
+
+# Performance comparison of 2-way vs 3-way mergesort
+
+
+def mergesort_test():
+    f = open('mergesort_two.txt', 'a')
+    for n in range(500):
+        values = []
+        for _ in range(100):
+            test_list = create_random_list(n+1)
+
+            start_time = timeit.default_timer()
+            mergesort(test_list)
+            end_time = timeit.default_timer()
+
+            values.append(end_time - start_time)
+        f.write(str(n+1) + '\t' + str(sum(values)/len(values)) + '\r')
+    f.close()
+
+
+def mergesort_three_test():
+    f = open('mergesort_three.txt', 'a')
+    for n in range(500):
+        values = []
+        for _ in range(100):
+
+            test_list = create_random_list(n+1)
+
+            start_time = timeit.default_timer()
+            mergesort_three(test_list)
+            end_time = timeit.default_timer()
+
+            values.append(end_time - start_time)
+
+        f.write(str(n+1) + '\t' + str(sum(values)/len(values)) + '\r')
+    f.close()
--- sorts.py
+++ sorts.py
@@ -1,67 +1,145 @@
 """ Start of functions we created"""
 
 # Merge Function (mergesort_bottom(L) helper)
-def merge_bottom(L, start, mid, end): 
-    
+
+
+def merge_bottom(L, start, mid, end):
+
     # Initalize lists to 0
     l_list = L[start:mid+1]
     r_list = L[mid+1:end+1]
-    
-    #Lenths of the list to merge
+
+    # Lenths of the list to merge
     l_len = len(l_list)
     r_len = len(r_list)
-    
+
     # Two pointers to keep track of location in both subarrays
     i, j = 0, 0
-    
-    #Construct sorted subarray
-    while i < l_len and j < r_len: 
-        
-        #Take element from left list if it is greater
-        if l_list[i] > r_list[j]: 
-            L[start] = r_list[j] 
+
+    # Construct sorted subarray
+    while i < l_len and j < r_len:
+
+        # Take element from left list if it is greater
+        if l_list[i] > r_list[j]:
+            L[start] = r_list[j]
             j += 1
-            
-        #Take element from right list if it is greater
-        else: 
-            L[start] = l_list[i] 
+
+        # Take element from right list if it is greater
+        else:
+            L[start] = l_list[i]
             i += 1
         start += 1
- 
+
     # Check if either list has left over elements
-    while i < l_len: 
-        L[start] = l_list[i] 
+    while i < l_len:
+        L[start] = l_list[i]
         i += 1
         start += 1
- 
-    while j < r_len: 
-        L[start] = r_list[j] 
+
+    while j < r_len:
+        L[start] = r_list[j]
         j += 1
         start += 1
 
 # Call this function to sort
+
+
 def mergesort_bottom(L):
-    
-    #Length of subarray
+
+    # Length of subarray
     length = len(L)
-    
-    #Length of subarrays to merge
+
+    # Length of subarrays to merge
     subarray_length = 1
-    
+
     while subarray_length < length:
- 
-        #Iterate to find bounds for subarrays
+
+        # Iterate to find bounds for subarrays
         for i in range(0, length - 1, 2*subarray_length):
             start = i
             mid = i + subarray_length - 1
             end = min(i + 2 * subarray_length - 1, length - 1)
-            
+
             # Only merge if valid bounds
             if start < length and mid < length and end < length:
                 merge_bottom(L, start, mid, end)
- 
+
         # Subarray length increases exponentianally (2^n)
         subarray_length *= 2
- 
+
+
+# Three-way mergesort
+def mergesort_three(L):
+    if len(L) <= 1:
+        return
+    mid1 = len(L)//3
+    mid2 = mid1*2
+    left, mid, right = L[:mid1], L[mid1:mid2], L[mid2:]
+    if len(L) <= 2:
+        left, mid, right = L[:1], [], L[1:]
+
+    # Mergesort core
+    mergesort_three(left)
+    mergesort_three(mid)
+    mergesort_three(right)
+    temp = merge_three(left, mid, right)
+
+    # Copy sorted list to L
+    for i in range(len(temp)):
+        L[i] = temp[i]
+
+
+def merge_three(left, mid, right):
+    L = []
+    i = j = k = 0
+
+    while i < len(left) or j < len(mid) or k < len(right):
+        if i >= len(left) and j >= len(mid):
+            L.append(right[k])
+            k += 1
+        elif i >= len(left) and k >= len(right):
+            L.append(mid[j])
+            j += 1
+        elif j >= len(mid) and k >= len(right):
+            L.append(left[i])
+            i += 1
+        elif i >= len(left):
+            if mid[j] <= right[k]:
+                L.append(mid[j])
+                j += 1
+            else:
+                L.append(right[k])
+                k += 1
+        elif j >= len(mid):
+            if left[i] <= right[k]:
+                L.append(left[i])
+                i += 1
+            else:
+                L.append(right[k])
+                k += 1
+        elif k >= len(right):
+            if mid[j] <= left[i]:
+                L.append(mid[j])
+                j += 1
+            else:
+                L.append(left[i])
+                i += 1
+        else:
+            if left[i] <= mid[j]:
+                if left[i] <= right[k]:
+                    L.append(left[i])
+                    i += 1
+                else:
+                    L.append(right[k])
+                    k += 1
+            else:
+                if mid[j] <= right[k]:
+                    L.append(mid[j])
+                    j += 1
+                else:
+                    L.append(right[k])
+                    k += 1
+    return L
+
 
 ''' End of functions we created'''
