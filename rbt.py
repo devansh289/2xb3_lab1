8707ff932170acdda521bf1409dfbd627f84dbd3
Rizwan Ahsan <rizahsan4@gmail.com>
2021-03-05 23:03:51 -0500

    bleh

--- rbt.py
+++ rbt.py
@@ -51,24 +51,38 @@ class RBNode:
         return "(" + str(self.value) + "," + self.colour + ")"
 
     def rotate_right(self):
-        x = self
         left_child = self.left
-        right_child = self.right
-        self.left = left_child.left
-        self.right = x
-        self.right.left = left_child.right
-        self.right.right = right_child
-        return left_child
+        if left_child.right is not None:
+            left_child.right.parent = self
+        if self.parent is not None:
+            left_child.parent = self.parent
+        self.parent = left_child
+        return self
+        # x = self
+        # left_child = self.left
+        # right_child = self.right
+        # self.left = left_child.left
+        # self.right = x
+        # self.right.left = left_child.right
+        # self.right.right = right_child
+        # return left_child
 
     def rotate_left(self):
-        x = self
-        left_child = self.left
         right_child = self.right
-        self.left = x
-        self.left.left = left_child
-        self.left.right = right_child.left
-        self.right = right_child.right
-        return right_child
+        if right_child.left is not None:
+            right_child.left.parent = self
+        if self.parent is not None:
+            right_child.parent = self.parent
+        self.parent = right_child
+        return self
+        # x = self
+        # left_child = self.left
+        # right_child = self.right
+        # self.left = x
+        # self.left.left = left_child
+        # self.left.right = right_child.left
+        # self.right = right_child.right
+        # return right_child
 
 
 class RBTree:
@@ -117,51 +131,55 @@ class RBTree:
         if node.parent == None:
             node.make_black()
         while node != None and node.parent != None and node.parent.is_red(): 
-            if node.get_uncle().is_red():
+            if node.get_uncle() is not None and node.get_uncle().is_red():
                 node.parent.make_black()
                 node.get_uncle().make_black()
                 node.parent.parent.make_red()
                 node = node.parent.parent
             else:
                 # left left case
-                if node.parent.left.is_red() and node.parent.parent.left.is_red():
-                    grandfather = node.parent.parent
-                    x = node.parent.parent.rotate_right()
-                    if grandfather == self.root:
-                        self.root = x
-                    x.make_black()
-                    x.right.make_red()
-                    node = node.parent
+                if node.parent.left != None and node.parent.parent.left != None:
+                    if node.parent.left.is_red() and node.parent.parent.left.is_red():
+                        grandfather = node.parent.parent
+                        x = node.parent.parent.rotate_right()
+                        if grandfather == self.root:
+                            self.root = x
+                        x.make_black()
+                        x.right.make_red()
+                        node = node.parent
 
                 # left right case
-                if node.parent.right.is_red() and node.parent.parent.left.is_red():
-                    node = node.parent.rotate_left()
-                    parent = node.parent
-                    node = node.parent.rotate_right()
-                    if parent == self.root:
-                        self.root = node
-                    node.make_black()
-                    node.right.make_red()
+                if node.parent.right != None and node.parent.parent.left != None:
+                    if node.parent.right.is_red() and node.parent.parent.left.is_red():
+                        node = node.parent.rotate_left()
+                        parent = node.parent
+                        node = node.parent.rotate_right()
+                        if parent == self.root:
+                            self.root = node
+                        node.make_black()
+                        node.right.make_red()
 
                 # right right case
-                if node.parent.right.is_red() and node.parent.parent.right.is_red():
-                    grandfather = node.parent.parent
-                    x = node.parent.parent.rotate_left()
-                    if grandfather == self.root:
-                        self.root = x
-                    x.make_black()
-                    x.left.make_red()
-                    node = node.parent
+                if node.parent.right != None and node.parent.parent.right != None:
+                    if node.parent.right.is_red() and node.parent.parent.right.is_red():
+                        grandfather = node.parent.parent
+                        x = node.parent.parent.rotate_left()
+                        if grandfather == self.root:
+                            self.root = x
+                        x.make_black()
+                        x.left.make_red()
+                        node = node.parent
 
                 # right left case
-                if node.parent.left.is_red() and node.parent.parent.right.is_red():
-                    node = node.parent.rotate_right()
-                    parent = node.parent
-                    node = node.parent.rotate_left()
-                    if parent == self.root:
-                        self.root = node
-                    node.make_black()
-                    node.left.make_red()
+                if node.parent.left != None and node.parent.parent.right != None:
+                    if node.parent.left.is_red() and node.parent.parent.right.is_red():
+                        node = node.parent.rotate_right()
+                        parent = node.parent
+                        node = node.parent.rotate_left()
+                        if parent == self.root:
+                            self.root = node
+                        node.make_black()
+                        node.left.make_red()
 
         self.root.make_black()
 
