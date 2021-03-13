cd3b9a38f849913b0d2782cb35c024f924aa67fa
Rizwan Ahsan <rizahsan4@gmail.com>
2021-03-12 21:45:59 -0500

    added test2

--- code.py
+++ code.py
@@ -5,12 +5,30 @@ import random
 def gen_random_graph(k, c):
     g = Graph(k)
     for i in range(c):
-        g.add_edge(random.randint(0, k), random.randint(0, k))
+        g.add_edge(random.randint(0, k-1), random.randint(0, k-1))
     return g
 
 
 def test():
     f = open("data.txt", "a")
+    for i in range(100):
+        my_graph = None
+        cycle = 0
+        no_cycle = 0
+        for _ in range(100):
+            my_graph = gen_random_graph(100, i)
+
+            if has_cycle(my_graph):
+                cycle += 1
+            else:
+                no_cycle += 1
+
+        f.write(f"{i}\t{cycle/(no_cycle+cycle)}\n")
+    f.close()
+
+
+def test2():
+    f = open("data2.txt", "a")
     for i in range(100):
         my_graph = None
         connected = 0
@@ -23,9 +41,5 @@ def test():
             else:
                 not_connected += 1
 
-        f.write(connected/(not_connected+connected))
+        f.write(f"{i}\t{connected / (not_connected + connected)}\n")
     f.close()
-
-
-# def cycle_v_c():
-#
