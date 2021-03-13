34e11d29a44750314bd5e27299f22e6823f30530
Devansh Sharma <devanshsharma289@gmail.com>
2021-03-12 20:30:01 -0500

    added test

--- code.py
+++ code.py
@@ -6,6 +6,25 @@ def gen_random_graph(k, c):
     g = Graph(k)
     for i in range(c):
         g.add_edge(random.randint(0, k), random.randint(0, k))
+    return g
+
+
+def test():
+    f = open("data.txt", "a")
+    for i in range(100):
+        my_graph = None
+        connected = 0
+        not_connected = 0
+        for _ in range(100):
+            my_graph = gen_random_graph(100, i)
+
+            if is_connected(my_graph):
+                connected += 1
+            else:
+                not_connected += 1
+
+        f.write(connected/(not_connected+connected))
+    f.close()
 
 
 # def cycle_v_c():
