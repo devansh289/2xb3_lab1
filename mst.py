5b61722b6c0f632d53ab0d73016aa08b88626230
Devansh Sharma <devanshsharma289@gmail.com>
2021-03-23 13:36:42 -0400

    Added prim2()

--- mst.py
+++ mst.py
@@ -45,49 +45,82 @@ def prim1(graph):
                 break
 
         vertices_visited += 1
+
+    # Uncomment to check graph
+    # print(ans.adj)
     return ans
 
 
 def prim2(graph):
     # Graph to output as answer
     ans = WeightedGraph(graph.number_of_nodes())
-    visited = [False] * graph.number_of_nodes()
+    visited = set()
 
     # Number of nodes visited
     vertices_visited = 0
 
     # Heap for current edges we can choose from
-    adjacent_edges = [Element(float('inf'), i)
+    adjacent_edges = [Element(i, 1001)
                       for i in range(graph.number_of_nodes())]
     adjacent_edges = MinHeap(adjacent_edges)
 
     # Node we are on
     current_node = adjacent_edges.extract_min().value
 
+    # Mark node as visited
+    visited.add(current_node)
+
     # While not all nodes are visited
     while vertices_visited < graph.number_of_nodes():
-        visited[current_node] = True
-
-        # Update heap with correct values
+        # Update heap with correct weights
         for edge in graph.adjacent_nodes(current_node):
             adjacent_edges.decrease_key(edge[0], edge[1])
 
-        ''' WHAT TO DO NEXT ?!?!?!'''
-
         chosen_edge = None
 
-        for edge in adjacent_edges:
-            chosen_edge = edge
-            edges = list(edge[1])
+        while not adjacent_edges.is_empty():
+            # Set current edge to min
+            chosen_edge = adjacent_edges.extract_min()
 
-            # Node which is not yet visited
-            new_node = edges[0] if visited[edges[1]] else edges[1]
+            # Information about chosen edge
+            node1 = chosen_edge.value
+            node2 = None
+            weight = chosen_edge.key
 
-            # If edge does not create loop
-            if not (visited[edges[0]] and visited[edges[1]]):
-                ans.add_edge(edges[0], edges[1], edge[0])
-                current_node = new_node
-                break
+            # Get node2 (other node that the edge connects)
+            for node in visited:
+                if graph.are_connected(node, node1):
+                    if graph.w(node, chosen_edge.value) == weight:
+                        node2 = node
+                        break
 
+            # Set new_node to the node that has not been visited yet
+            new_node = None
+            if node1 in visited:
+                new_node = node2
+            else:
+                new_node = node1
+
+            # If there is a valid node2, else fetch least value again
+            if node2 != None:
+
+                # Make sure both nodes weren't visited to avoid loops
+                if not (node1 in visited and node2 in visited):
+                    ans.add_edge(node1, node2, weight)
+                    visited.add(node1)
+                    visited.add(node2)
+
+                    # List to append new edges to
+                    L = []
+                    for edge in graph.adjacent_nodes(new_node):
+                        L.append(Element(edge[0], edge[1]))
+
+                    # Add new possible edges
+                    adjacent_edges.insert_elements(L)
+                    current_node = new_node
+                    break
         vertices_visited += 1
+
+    # Uncomment to check graph
+    # print(ans.adj)
     return ans
